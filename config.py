872fc533c3e1a534fd86e69666fd7766c6709f1b
Mahesh-Saravanan <maheshhss750@gmail.com>
2025-02-16 20:55:51 +0530

    updated

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+import torch
+
+class Config:
+    
+    data_dir = "/Users/maheshsaravanan/Desktop/FaceID/DS"
+
+    
+    image_size = 512         
+    batch_size = 16
+    num_epochs = 50
+    learning_rate = 1e-3
+    latent_dim = 512
+    num_workers = 4
+    checkpoint_path ="checkpoints"
+    SaveInterval = 2
+    image_path = "/Users/maheshsaravanan/Desktop/FaceID/TestFolder/test1.jpg"
+
+    Encoder_path = "./checkpoints/encoder_at_Epoch5.pth"
+    Decoder_path = "./checkpoints/decoder_at_Epoch5.pth"
+   
+    split_ratios = (0.85, 0.10, 0.05)
+    device ="mps" if torch.backends.mps.is_available() else "cpu"
